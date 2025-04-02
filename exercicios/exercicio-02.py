796a4178558ca5a015711331ecb64cbc3dcff300
Tony Vinicius Cavalcante <82682652+tonyviniciuscavalcante@users.noreply.github.com>
2025-04-02 14:58:36 -0300

    Update exercicio-02.py

--- exercicios/exercicio-02.py
+++ exercicios/exercicio-02.py
@@ -29,3 +29,66 @@ musica2 = Musica(nome='The Trooper', artista='Iron Maiden', duracao=245)
 musica3 = Musica(nome='Hotel California', artista='Eagles', duracao=390)
 
 Musica.listar_musicas()
+
+
+# Exercícios
+# 1. Implemente uma classe chamada Carro com os atributos básicos, como modelo, cor e ano. Crie uma instância dessa classe e atribua valores aos seus atributos.
+class Carro:
+    def __init__(self, modelo, cor, ano):
+        self.nome = modelo
+        self.cor = cor
+        self.ano = ano
+
+meu_carro = Carro(modelo='Fusca', cor='Azul', ano=1970)
+
+# 2. Crie uma classe chamada Restaurante com os atributos nome, categoria, ativo e crie mais 2 atributos. Instancie um restaurante e atribua valores aos seus atributos.
+class Restaurante:
+    def __init__(self, nome, categoria, capacidade, nota_avaliacao, ativo=False):
+        self.nome = nome
+        self.categoria = categoria
+        self.ativo = ativo
+        self.capacidade = capacidade
+        self.nota_avaliacao = nota_avaliacao
+
+restaurante_exemplo = Restaurante(nome='Comida Boa', categoria='Gourmet', ativo=True, capacidade=50, nota_avaliacao=4.5)
+
+# 3. Modifique a classe Restaurante adicionando um construtor que aceita nome e categoria como parâmetros e inicia ativo como False por padrão.
+# Crie uma instância utilizando o construtor.
+class Restaurante:
+    def __init__(self, nome, categoria, capacidade, nota_avaliacao, ativo=False):
+        self.nome = nome
+        self.categoria = categoria
+        self.ativo = ativo
+        self.capacidade = capacidade
+        self.nota_avaliacao = nota_avaliacao
+
+novo_restaurante = Restaurante(nome='Santa Marmita', categoria='Fast Food', capacidade=50, nota_avaliacao=4.5)
+
+# 4. Adicione um método especial __str__ à classe Restaurante para que, ao imprimir uma instância, seja exibida uma mensagem formatada com o nome e a categoria.
+# Exiba essa mensagem para uma instância de restaurante.
+class Restaurante:
+    def __init__(self, nome, categoria, capacidade, nota_avaliacao, ativo=False):
+        self.nome = nome
+        self.categoria = categoria
+        self.ativo = ativo
+        self.capacidade = capacidade
+        self.nota_avaliacao = nota_avaliacao
+
+    def __str__(self):
+        return f'{self.nome} | {self.categoria}'
+
+restaurante_formatado = Restaurante(nome='Bom Sabor', categoria='Tradicional', capacidade=50, nota_avaliacao=4.5)
+print(restaurante_formatado)
+
+# 5. Crie uma classe chamada Cliente e pense em 4 atributos. Em seguida, instancie 3 objetos desta classe e atribua valores aos seus atributos
+# através de um método construtor.
+class Cliente:
+    def __init__(self, nome='', idade=0, email='', telefone=''):
+        self.nome = nome
+        self.idade = idade
+        self.email = email
+        self.telefone = telefone
+
+cliente1 = Cliente(nome='Alice', idade=25, email='alice@gmail.com', telefone='123-456-7890')
+cliente2 = Cliente(nome='Bob', idade=30, email='bob@gmail.com', telefone='987-654-3210')
+cliente3 = Cliente(nome='Charlie', idade=22, email='charlie@gmail.com', telefone='555-123-4567')
